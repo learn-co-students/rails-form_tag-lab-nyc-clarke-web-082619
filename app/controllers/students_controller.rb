5c1e99b260c86adf66cac00eb006c1c529edf737
github username <github email address>
2019-09-20 11:51:37 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new 
+  end
+
+  def create
+    redirect_to student_path(Student.create(student_params))
+  end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
 end
